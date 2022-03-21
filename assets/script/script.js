4eeecb221ec3fa80c6e8d435516174fe1c954a5a
Eulalia Pirone <98905459+eulaliapi@users.noreply.github.com>
2022-03-21 12:20:30 +0100

    index.html and script.js updated

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,12 +1,29 @@
-const increaseBtn = document.querySelector('.increase');
-const decreaseBtn = document.querySelector('.decrease');
-const startNumber = document.querySelector('.start-number');
+const generalBox = document.createElement('div');
+generalBox.className = 'counter';
+const title = document.createElement('h1');
+title.innerHTML = "Current count:";
+const divCounterBox = document.createElement('div');
+divCounterBox.className = 'counter-box';
+const increaseBtn = document.createElement('button');
+increaseBtn.className = 'increase';
+increaseBtn.innerHTML = "+";
+const decreaseBtn = document.createElement('button');
+decreaseBtn.className = 'decrease';
+decreaseBtn.innerHTML = "-";
+const startNumber = document.createElement('p');
+startNumber.className = 'start-number';
+startNumber.innerHTML = "0";
 
+document.body.appendChild(generalBox);
+generalBox.appendChild(title);
+generalBox.appendChild(divCounterBox);
+divCounterBox.appendChild(increaseBtn);
+divCounterBox.appendChild(startNumber);
+divCounterBox.appendChild(decreaseBtn);
 
 increaseBtn.addEventListener('click', function() {
     startNumber.innerText++
 });
-
 decreaseBtn.addEventListener('click', function() {
     startNumber.innerText--
 });
